84ad9b0f566a6d956adba07517c491285bd39ad1
JiHongKim98 <kinjihong9598@gmail.com>
2024-03-05 00:59:02 +0900

    feat: Add jwt token response

--- src/main/java/com/example/oauth2/auth/presentation/AuthController.java
+++ src/main/java/com/example/oauth2/auth/presentation/AuthController.java
@@ -1,12 +1,15 @@
 package com.example.oauth2.auth.presentation;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.oauth2.auth.application.AuthService;
+import com.example.oauth2.auth.application.dto.TokenResponse;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,13 +22,13 @@ public class AuthController {
 
 	private final AuthService authService;
 
-	// TODO: 반환값 void -> JWT 토큰
+	@ResponseStatus(HttpStatus.OK)
 	@GetMapping("/{socialType}/callback")
-	public void loginOrCreateMember(
+	public TokenResponse loginOrCreateMember(
 		@PathVariable("socialType") String socialType,
 		@RequestParam("code") String code
 	) {
 		log.info("socialType = {}, code = {}", socialType, code);
-		authService.loginOrCreateMember(socialType, code);
+		return authService.loginOrCreateMember(socialType, code);
 	}
 }
