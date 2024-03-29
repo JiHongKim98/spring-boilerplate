11d6fb55fab778de2057669c54f44cc2b9d4ff37
JiHongKim98 <kinjihong9598@gmail.com>
2024-03-30 01:14:55 +0900

    feat: Add request argument validator and exception

--- src/main/java/com/example/oauth2/auth/application/dto/ReissueRequest.java
+++ src/main/java/com/example/oauth2/auth/application/dto/ReissueRequest.java
@@ -1,6 +1,8 @@
 package com.example.oauth2.auth.application.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record ReissueRequest(
-	String refreshToken
+	@NotNull String refreshToken
 ) {
 }
--- src/main/java/com/example/oauth2/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/oauth2/common/exception/GlobalExceptionHandler.java
@@ -1,14 +1,21 @@
 package com.example.oauth2.common.exception;
 
+import java.util.Objects;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(BaseException.class)
 	public ResponseEntity<ExceptionResponse> handleException(BaseException ex) {
@@ -25,5 +32,19 @@ public class GlobalExceptionHandler {
 
 		return ResponseEntity.internalServerError()
 			.body(new ExceptionResponse("INTERNAL_SERVER_ERROR", ex.getMessage()));
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+		MethodArgumentNotValidException ex,
+		HttpHeaders headers,
+		HttpStatusCode status,
+		WebRequest request
+	) {
+		log.warn(ex.getMessage(), ex);
+
+		String message = Objects.requireNonNull(ex.getBindingResult().getFieldError()).getDefaultMessage();
+		return ResponseEntity.status(status)
+			.body(new ExceptionResponse("ARGUMENT_NOT_VALID", message));
 	}
 }
