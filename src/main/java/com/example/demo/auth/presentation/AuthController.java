7691b1d9f1a813442906e14a2b7ab9664bc6b6ca
JiHongKim98 <kinjihong9598@gmail.com>
2024-07-21 19:50:14 +0900

    refactor: Switch to cookie-based refresh token handling #20
    * Closes #20

--- src/main/java/com/example/demo/auth/presentation/AuthController.java
+++ src/main/java/com/example/demo/auth/presentation/AuthController.java
@@ -1,21 +1,23 @@
 package com.example.demo.auth.presentation;
 
+import static org.springframework.http.HttpHeaders.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.auth.application.AuthFacadeService;
-import com.example.demo.auth.application.dto.LogoutRequest;
-import com.example.demo.auth.application.dto.ReissueRequest;
 import com.example.demo.auth.application.dto.TokenResponse;
 import com.example.demo.common.annotation.Auth;
+import com.example.demo.common.presentation.cookie.CookieHandler;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -23,29 +25,43 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
+	private static final String COOKIE_REFRESH_TOKEN = "refresh_token";
+
 	private final AuthFacadeService authFacadeService;
+	private final CookieHandler cookieHandler;
 
 	@GetMapping("/{socialType}/callback")
 	public ResponseEntity<TokenResponse> loginOrRegister(
 		@PathVariable("socialType") String socialType,
 		@RequestParam("code") String code
 	) {
-		return ResponseEntity.ok(authFacadeService.loginOrRegister(socialType, code));
+		TokenResponse tokenResponse = authFacadeService.loginOrRegister(socialType, code);
+		ResponseCookie cookie = cookieHandler.createCookie(COOKIE_REFRESH_TOKEN, tokenResponse.refreshToken());
+		return ResponseEntity.status(HttpStatus.OK)
+			.header(SET_COOKIE, cookie.toString())
+			.body(tokenResponse);
 	}
 
 	@GetMapping("/reissue")
 	public ResponseEntity<TokenResponse> reissueToken(
-		@RequestBody @Valid ReissueRequest request
+		@CookieValue(COOKIE_REFRESH_TOKEN) String refreshToken
 	) {
-		return ResponseEntity.ok(authFacadeService.reissueToken(request));
+		TokenResponse tokenResponse = authFacadeService.reissueToken(refreshToken);
+		ResponseCookie cookie = cookieHandler.createCookie(COOKIE_REFRESH_TOKEN, tokenResponse.refreshToken());
+		return ResponseEntity.status(HttpStatus.OK)
+			.header(SET_COOKIE, cookie.toString())
+			.body(tokenResponse);
 	}
 
 	@PostMapping("/logout")
 	public ResponseEntity<Void> logout(
 		@Auth Long memberId,
-		@RequestBody @Valid LogoutRequest request
+		@CookieValue(COOKIE_REFRESH_TOKEN) String refreshToken
 	) {
-		authFacadeService.logout(memberId, request);
-		return ResponseEntity.ok().build();
+		authFacadeService.logout(memberId, refreshToken);
+		ResponseCookie cookie = cookieHandler.deleteCookie(COOKIE_REFRESH_TOKEN);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT)
+			.header(SET_COOKIE, cookie.toString())
+			.build();
 	}
 }
