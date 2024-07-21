6f2a2c6862eb57a0071a1ca139901fa57efa128e
JiHongKim98 <kinjihong9598@gmail.com>
2024-07-21 19:42:24 +0900

    feat: Add cookie handler class #20

---
+++ src/main/java/com/example/demo/common/presentation/cookie/CookieProperties.java
@@ -0,0 +1,13 @@
+package com.example.demo.common.presentation.cookie;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.cookie")
+public record CookieProperties(
+	Long maxAge,
+	String path,
+	String sameSite,
+	boolean httpOnly,
+	boolean secure
+) {
+}
